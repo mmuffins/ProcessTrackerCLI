c8cc0f0adf1e10fe16992166c494cff7e701084e
Michael Kellner <mail@tiv73.com>
2024-09-22 10:27:35 +0200

    Reorganize project (#1)
    * Remove unused project
    
    * Reorganize project
    
    * Update dependencies
    
    * Fix date parsing bug
    
    * Add new project

---
+++ ProcessTrackerCLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessTrackerCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
