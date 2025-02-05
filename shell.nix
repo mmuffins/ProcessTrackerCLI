c59dc9810d8242fef9e8bdefc77acdd91c8241b9
mmuffins <mail@tiv73.com>
2025-02-05 22:06:46 +0100

    Add nix flake

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  buildInputs = with pkgs; [
+    dotnetCorePackages.sdk_9_0
+    omnisharp-roslyn
+    nuget-to-json
+    # stdenv.cc
+  ];
+in
+  pkgs.mkShell {
+  inherit buildInputs;
+    name = "dotnet-env";
+    DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_9_0}";
+
+    shellHook = ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
+      exec zsh
+    '';
+}
