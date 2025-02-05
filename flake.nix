002e28a33cfeff80d7a3ce7d5716ae0cccc69814
Michael Kellner <mail@tiv73.com>
2025-02-05 22:26:14 +0100

    Add nix support (#7)
    * Update project to .net 9
    
    * Add nix support

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "process-tracker-cli";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { 
+    self, 
+    nixpkgs, 
+    ...  
+  } @ inputs: let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      appVersion = "1.0.111";
+      dotnetVersion = "9_0";
+    in {
+      inherit system;
+
+      packages."${system}" = {
+        process-tracker = pkgs.buildDotnetModule rec {
+          pname = "process-tracker-cli";
+          version = "${appVersion}";
+
+          meta = with pkgs.lib; {
+            description = "A cross-platform tool to track and report running processes";
+            license = licenses.mit;
+            platforms = [ system ];
+          };
+
+          dotnet-sdk = pkgs.dotnetCorePackages."sdk_${dotnetVersion}";
+          dotnet-runtime = pkgs.dotnetCorePackages."runtime_${dotnetVersion}";
+
+          src = self;
+
+          projectFile = [
+            "ProcessTracker/ProcessTracker.csproj"
+          ];
+
+          # to manually update dependencies:
+          # dotnet restore --use-current-runtime --packages nuget-restore ./ProcessTrackerCLI.sln
+          # nuget-to-json nuget-restore > deps.json
+          # rm -r nuget-restore
+          nugetDeps = ./deps.json;
+          executables = [ "processtrackercli" ];
+        };
+      };
+
+      defaultPackage."${system}" = self.packages."${system}".process-tracker;
+
+      nixosModules.process-tracker-cli = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.process-tracker-cli;
+        in {
+          options.programs.process-tracker = {
+            enable = lib.mkEnableOption "Enable the process tracker cli";
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${system}.process-tracker;
+              description = "The package to install as the process tracker cli.";
+            };
+            # Extra service options (if needed)
+            # serviceConfig = lib.mkOption {
+            #   type = lib.types.attrs;
+            #   default = {};
+            #   description = "Extra configuration options for the process tracker systemd unit.";
+            # };
+          };
+
+          config = lib.mkIf cfg.enable {
+            home.packages = [ cfg.package ];
+          };
+        };
+  };
+}
